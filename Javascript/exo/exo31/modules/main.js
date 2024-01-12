5bb9b3f2d87a3b78624f4ec06fad77151648dc01
Oceane <garciaoceane3122@gmail.com>
2024-01-12 16:26:54 +0100

    exo31

---
+++ Javascript/exo/exo31/modules/main.js
@@ -0,0 +1,4 @@
+/*Créer une classe **Client** avec gestion d'articles : Créez une classe **Client** qui peut **ajouter des articles** à un panier d'achat. 
+Créez également une classe **Article** avec des propriétés telles que le **nom**, le **prix**, et la **quantité en stock**.
+ Permettez au client de **passer une commande** et de **mettre à jour le stock d'articles**.*/
+
