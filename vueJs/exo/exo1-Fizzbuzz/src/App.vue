8995931c0e142e8424996a0f8058c791f16d505c
Oceane <garciaoceane3122@gmail.com>
2024-01-22 20:35:26 +0100

    Fizzbuzz

---
+++ vueJs/exo/exo1-Fizzbuzz/src/App.vue
@@ -0,0 +1,34 @@
+<!-- # Exercice 01
+
+Réaliser, via le framework Vue.js, un compteur FizzBuzz
+
+Vous devrez respecter les règles du FizzBuzz qui sont les suivantes: 
+
+* Si le nombre est un multiple de 3, on affiche 'Fizz'
+* Si le nombre est un multiple de 5, on affiche 'Buzz'
+* Si le nombre est un multiple de 3 et de 5, on affiche 'FizzBuzz'
+* Si le nombre est un nombre quelconque, on affiche seulement la valeur du nombre
+
+Pour cela, vous ferez un composant FizzBuzz qui contiendra un span et deux boutons. 
+* Le span devra afficher, via une propriété de type `computed`, la valeur du compteur en version FizzBuzz
+* Un bouton pour incrémenter la valeur du compteur
+* Un bouton pour décrémenter la valeur du compteur
+
+Il faudra empêcher l'utilisateur d'avoir une valeur inférieure à 0 via un v-on:disabled se basant sur soit une valeur computed, soit directement sur la valeur du compteur.
+
+Pour la valeur du compteur, pensez à faire usage d'une variable réactive via `ref()` -->
+
+<script setup>
+  import FizzBuzz from './components/Fizzbuzz.vue'
+
+</script>
+
+<template>
+  
+  <FizzBuzz/>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ vueJs/exo/exo1-Fizzbuzz/src/components/Fizzbuzz.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+const counter = ref(0);
+
+const fizzBuzzValue = computed(() => {
+  if (counter.value % 3 === 0 && counter.value % 5 === 0) {
+    return 'FizzBuzz';
+  } else if (counter.value % 3 === 0) {
+    return 'Fizz';
+  } else if (counter.value % 5 === 0) {
+    return 'Buzz';
+  } else {
+    return counter.value;
+  }
+});
+
+const increment = () => {
+  counter.value++;
+};
+
+const decrement = () => {
+  if (counter.value > 0) {
+    counter.value--;
+  }
+};
+</script>
+
+<template>
+
+<div>
+    <span>{{ fizzBuzzValue }}</span>
+    <button @click="increment" :disabled="counter.value <= 0">Increment</button>
+    <button @click="decrement" :disabled="counter.value <= 0">Decrement</button>
+</div>
+
+</template>
+
+<script scoped>
+
+// button {
+//         background-color: rgb(144, 17, 17);
+//     }
+    
+
+</script>
