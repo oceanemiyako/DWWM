d2429efe141c259e9d9d6c3727f24706f92cd989
Oceane <garciaoceane3122@gmail.com>
2024-01-23 12:22:08 +0100

    signin

---
+++ vueJs/exo/02-sign-in/src/App.vue
@@ -0,0 +1,25 @@
+<!-- /*# Exercice 02 
+
+Réaliser en Vue.js un formulaire de Sign In. 
+
+Ce formulaire devra demander aux utilisateurs leur email et leur mot de passe. 
+
+Le formulaire disposera de vérification en temps réel: 
+    * Les inputs invalides seront colorés en rouge pâle
+    * Des spans apparaitront en dessous des inputs n'ayant pas de valeur correcte (après modification, le champs reste vide par exemple ou il ne s'agit pas d'un email valide). Leur CSS les fera apparaitre en rouge gras
+    * Il ne sera pas possible d'envoyer un formulaire en cas de présence dans son intérieur d'un champs invalide
+
+Lors de l'envoie d'un formulaire par l'utilisateur, il y aura affichage en console d'un objet de crédentials qui contiendra l'email et le mot de passe de l'utilisateur en tant qu'attributs.*/ -->
+
+
+<script setup>
+import SignIn from './components/SignIn.vue';
+</script>
+
+<template>
+<SignIn/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vueJs/exo/02-sign-in/src/components/SignIn.vue
@@ -0,0 +1,51 @@
+<script setup>
+import {ref, computed } from 'vue';
+const credentials = ref({
+email: '',
+password: ''
+});
+
+const isEmailValid = computed(() => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(credentials.value.email));
+const isPasswordValid = computed(() => credentials.value.password.length >= 6);
+const isFormValid = computed(() => isEmailValid.value && isPasswordValid.value);
+
+const submitForm = () => {
+if (isFormValid.value) {
+    console.log('Credentials:', credentials.value);
+}
+};
+
+</script>
+
+<template>
+<div>
+    <form>
+
+<div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" v-model="credentials.email" :class="{ 'invalid-input': !isEmailValid }">
+        <span v-if="!isEmailValid" class="error-message">Veuillez saisir une adresse e-mail valide</span>
+</div>
+
+<div>
+        <label for="password">Mot de passe:</label>
+        <input type="password" id="password" v-model="credentials.password" :class="{ 'invalid-input': !isPasswordValid }">
+        <span v-if="!isPasswordValid" class="error-message">Veuillez saisir un mot de passe valide</span>
+</div>
+
+<button type="submit" :disabled="!isFormValid">Se connecter</button>
+    </form>
+</div>
+
+</template>
+
+<style scoped>
+.invalid-input {
+    border: 1px solid #ff9999;
+}
+
+.error-message {
+    color: #ff0000;
+    font-weight: bold;
+}
+</style>
